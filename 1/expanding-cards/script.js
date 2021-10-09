b38c67f636e517ad703ebdec60a15f5c68d5e901
msmanawadu <msmanawadu@gmail.com>
2021-10-09 21:30:16 +0530

    Complete Project 1

--- 1/expanding-cards/script.js
+++ 1/expanding-cards/script.js
@@ -1,6 +1,8 @@
+// pulling-in DOM elements
 const panels = document.querySelectorAll('.panel')
 
 panels.forEach(panel => {
+    // Wiring up an event listener for click event on each panel
     panel.addEventListener('click', () => {
         removeActiveClasses()
         panel.classList.add('active')
