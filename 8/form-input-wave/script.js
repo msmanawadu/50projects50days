229e1eb72747dcb3fc620ccbe096255c31f9ed0b
msmanawadu <msmanawadu@gmail.com>
2021-10-14 20:24:31 +0530

    Complete Project 8

---
+++ 8/form-input-wave/script.js
@@ -0,0 +1,9 @@
+const labels = document.querySelectorAll('.form-control label')
+
+// Apply the transitions animation dynamically
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+        .split('')
+        .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+        .join('')
+})
